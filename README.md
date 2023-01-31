4a22fe5f9d4fa37f64f6210eec928e77338182fd
gabriele.capobianco <gabriele.capobianco@addvalue.it>
2023-02-01 00:38:25 +0100

    [UPDATE] - 'fix logging: add trace sql queries,             update README.md'

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ Todo List App with Java FX + Springboot
 
 
 Try to follow hexagonal architecture idea
+
+
+# modules:
+- Parent  pom
+- Core    module
+- War     module
+- View    module
+- Project builder
+
+
--- view/src/main/java/dev/jeici/DataInitializer.java
+++ view/src/main/java/dev/jeici/DataInitializer.java
@@ -2,26 +2,27 @@ package dev.jeici;
 
 import dev.jeici.entities.User;
 import dev.jeici.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public
 class DataInitializer
 {
 	@Autowired
-	private
-	UserRepository repo;
+	private UserRepository repo;
 
 	@Bean
 	public
-	CommandLineRunner getCommandLineRunner() {
-		return args -> {
-			repo.save(User.builder().email("test_mail1").password("psw1").build());
-			repo.save(User.builder().email("test_mail2").password("psw2").build());
-			repo.save(User.builder().email("test_mail3").password("psw3").build());
-		};
+	CommandLineRunner getCommandLineRunner()
+	{
+		return args -> log.info("data user init: \nuser1: {},\nuser2: {},\nuser3: {}.",
+	                        repo.save(User.builder().email("test_mail1").password("psw1").build()),
+	                        repo.save(User.builder().email("test_mail2").password("psw2").build()),
+	                        repo.save(User.builder().email("test_mail3").password("psw3").build()));
 	}
 }
